c4f01dd5f53aa978476d7822050b0f4d4202a2f3
Raim <rai.interiano@gmail.com>
2015-11-11 18:47:08 -0600

    Project creation; C::B project for the mapping, with Allegro 5

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+#include <allegro5/allegro.h>
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
+
+using namespace std;
+
+/**
+    Inicialización de las funciones de Allegro
+**/
+int initAllegro()
+{
+    if(!al_init())
+    {
+        cout<<"failed to initialize allegro!\n"<<endl;
+        return -1;
+    }
+
+    if(!al_init_image_addon())
+    {
+        cout<<"Error: Failed to initialize al_init_image_addon!"<<endl;
+        return -1;
+    }
+
+    display = al_create_display(width, height);
+    if(!display)
+    {
+        cout<<"failed to create display!\n"<<endl;
+        return -1;
+    }
+
+    event_queue = al_create_event_queue();
+    if(!event_queue)
+    {
+        cout<<"failed to create event_queue!\n"<<endl;
+        al_destroy_display(display);
+        return -1;
+    }
+
+    timer = al_create_timer(1.0 / FPS);
+    if(!timer)
+    {
+        cout<<"failed to create timer!"<<endl;
+    }
+
+    if(!al_install_keyboard())
+    {
+        cout<<"failed to initialize the keyboard!"<<endl;
+    }
+
+    if(!al_install_audio() || !al_init_acodec_addon() || !al_reserve_samples(2))
+    {
+        cout<<"failed to initialize Audio!"<<endl;
+    }
+
+    al_init_font_addon(); // initialize the font addon
+    al_init_ttf_addon();// initialize the ttf (True Type Font) addon
+
+    if (!normalFont || !cartoonFont)
+    {
+        cout<<"Failed to initialize the font"<<endl;
+        return -1;
+    }
+
+    al_register_event_source(event_queue, al_get_display_event_source(display));//registrar eventos del display
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));//registrar eventos del timer
+    al_register_event_source(event_queue, al_get_keyboard_event_source());//registrar eventos del teclado
+
+    al_init_timeout(&timeout, 0.06);
+    return 0;
+}
+
+
+int main()
+{
+    if (initAllegro()<0)
+        return -1;
+    return 0;
+}
+
+/**
+    Una clase para el Path (el arreglo para ir appending las coordenadas).
+    {
+        funciones:
+            append(), remove(), insert(), draw(), clear();
+    }
+    Tener un path loader, del archivo que creó el Arduino del laberinto.
+
+    Un struct para coordenadas.
+    Constante para el width del rectángulo a dibujar.
+**/
