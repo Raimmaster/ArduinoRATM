18345bac02912778fb0a005328886e52f6d51a2f
Raimmaster <rai.interiano@gmail.com>
2015-11-29 17:23:34 -0600

    Separated everything into functions; can use now up to three sensors, not just one

--- Arduino/doppler_sensor/doppler_sensor.ino
+++ Arduino/doppler_sensor/doppler_sensor.ino
@@ -1,11 +1,9 @@
-a termin/* HC-SR04 Sensor
- *  
-
+/* HC-SR04 Sensor
+ * 
   ***EL CIRCUITO***
   *VCC conectado a POWER +5V
   *GND conectado a GND
-  *TRIG conectado al pin digital 2
-  *ECHO conectado al pin digital 4
+  *Revisar abajo los pines individuales
      
    Original code for Ping))) example was created by David A. Mellis
    Adapted for HC-SR04 by Tautvidas Sipavicius
@@ -14,49 +12,69 @@ a termin/* HC-SR04 Sensor
    This code example by flakas in: https://gist.github.com/flakas/3294829#file-hc-sr04-ino
 */
 
-const int trigPin = 2;
-const int echoPin = 4;
+//Frontal sensor, pines
+const int f_trigPin = 2;
+const int f_echoPin = 4;
+//Left sensor, pines
+const int l_trigPin = 6;
+const int l_echoPin = 8;
+//Right sensor, pines
+const int l_trigPin = 10;
+const int l_echoPin = 12;
 
 void setup() {
-  // inicializando comunicación serial
   Serial.begin(9600);
 }
 
 void loop()
-{
-  long duration, cm;
-
-  //El sensor es activado por un pulso HIGH de 10 o más µs
-  //Damos un pulso LOW Primero para asegurar un pulso HIGH limpio
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  //Leemos la señal del sensor: un pulso HIGH cuya duración
-  //es el tiempo en µs del ping enviado a la recepción del eco
-  //del objeto
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-
-  // convertir el tiempo en distancia
-  cm = microsecondsToCentimeters(duration);
-  int l_byte = cm % 256;
-  int h_byte = cm / 256;
-  //Mandar la distancia en dos bytes para que
-  //el puerto serial pueda manejar el tamaño
-  Serial.write(l_byte);
-  Serial.write(h_byte);
-  //Serial.print(cm);
-  //Serial.println(" cm");
-  
+{  
+  //Frontal
+  writeDistance(getDistance(f_trigPin, f_echoPin));  
+  //Left
+  writeDistance(getDistance(l_trigPin, l_echoPin));
+  //Right  
+  writeDistance(getDistance(r_trigPin, r_echoPin));  
   delay(1000);
 }
 
+/**
+ *Escribe la distancia recibida 
+ *a través del puerto serial
+ */
+void writeDistance(int distance){
+  //Mandar la distancia en dos bytes para que
+  //el puerto serial pueda manejar el tamaño
+  int l_byte = distance % 256;
+  int h_byte = distance / 256;
+    
+  Serial.write(l_byte);
+  Serial.write(h_byte);
+}
 
-long microsecondsToCentimeters(long microseconds)
+/**
+ * Obtener la distancia recorrida
+ */
+int getDistance(int trig_pin, int echo_pin){
+  //El sensor es activado por un pulso HIGH de 10 o más µs
+  //Damos un pulso LOW Primero para asegurar un pulso HIGH limpio
+  int duration = 0;
+  pinMode(l_trigPin, OUTPUT);
+  digitalWrite(l_trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(l_trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(l_trigPin, LOW);
+  
+  //Leemos la señal del sensor: un pulso HIGH cuya duración
+  //es el tiempo en µs del ping enviado a la recepción del eco
+  //del objeto
+  pinMode(l_echoPin, INPUT);
+  duration = pulseIn(l_echoPin, HIGH);    
+  // convertir el tiempo en distancia
+  return microsecondsToCentimeters(duration);
+}
+
+int microsecondsToCentimeters(int microseconds)
 { 
   //Velocidad del sonido es 340 m/s o 29.4118 microsegundos por centímetro
   //Encontramos la mitad del tiempo (dividimos entre 58) porque 
